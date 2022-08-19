d4feed4ec2239c38cdcd765f1ecee2785e996689
Phanupong Permpimol <phanupong@irobust.co.th>
2022-08-19 14:45:00 +0700

    create new produt page

---
+++ src/app/products/new-product.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IProduct, IProductForm } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  selector: 'app-new-product',
+  templateUrl: './new-product.component.html',
+  styleUrls: ['./new-product.component.css']
+})
+export class NewProductComponent{
+
+  addProductSuccess= false;
+  productResponse!: IProduct;
+
+  constructor(private productService: ProductService,
+              private router: Router){}
+
+  @ViewChild(NgForm) frmNewProduct!: NgForm;
+
+  newProduct: IProductForm = {
+    productName: '',
+    productCode: '',
+    price: 0,
+    starRating: 0
+  };
+
+  save(){
+    if(this.frmNewProduct.valid){
+      this.productService.addProduct(this.newProduct)
+          .subscribe(
+            (result) => this.handleResponse(result as IProduct)
+          );
+    }
+  }
+
+  handleResponse(result: IProduct){
+    if(result.id){
+      this.productResponse = result;
+      this.addProductSuccess = true;
+    }
+  }
+}
